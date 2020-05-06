fe541a60bea0ccaa38ee4d07b6e938a4874ea4d8
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-05-06 12:41:53 -0400

    Updates

---
+++ precache-manifest.cdcc5463c77f464c70dc2e7179b4e353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f934358f405e1e367d568b18296e222",
+    "url": "/rolodex-react-app/index.html"
+  },
+  {
+    "revision": "1e1a1a69df1f783fd338",
+    "url": "/rolodex-react-app/static/css/main.4c4af101.chunk.css"
+  },
+  {
+    "revision": "2c9c3ab47ba839742f42",
+    "url": "/rolodex-react-app/static/js/2.7025b507.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex-react-app/static/js/2.7025b507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1a1a69df1f783fd338",
+    "url": "/rolodex-react-app/static/js/main.3c8d9b87.chunk.js"
+  },
+  {
+    "revision": "44f4cb9c5f05c5ad23ca",
+    "url": "/rolodex-react-app/static/js/runtime-main.0a810ee2.js"
+  }
+]);
